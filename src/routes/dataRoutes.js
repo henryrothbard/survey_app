cdca331eeb717ef0579cd40a49af9a334fddf616
henryrothbard <henryrothbard24@gmail.com>
2024-05-20 17:52:14 -0400

    commit

--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -1,27 +1,28 @@
+const redisClient = require('../config/redisConfig');
 const express = require('express');
 const router = express.Router();
 const asyncHandler = require('../utils/errorWrapper');
-const redisClient = require('../config/redisConfig');
 
 router.post('/exists', asyncHandler(async (req, res) => {
-    const { uid } = req.body;
-    if (typeof uid !== 'string') {
-        res.status(400).send('Bad Request');
-        return;
-    }
+    const uid = String(req.body.uid);
+
     const value = await redisClient.get(uid);
+
     res.send({uid, exists: !!value});
 }));
 
 router.post('/change', asyncHandler(async (req, res) => {
-    const { uid, value } = req.body;
-    if (typeof uid !== 'string' || typeof value !== 'number') {
-        res.status(400).send('Bad Request');
+    const uid = String(req.body.uid);
+    const value = Number(req.body.value);
+
+    if (!(value >= process.env.MIN_OPT && value <= process.env.MAX_OPT)) {
+        res.status(400).send("Invalid Data."); 
         return;
     }
+
     await redisClient.set(uid, value);
+
     res.send({uid, value})
 }));
-
 
 module.exports = router;
