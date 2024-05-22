261cab6837438140de83e88eefd9932c6bba117d
henryrothbard <henryrothbard24@gmail.com>
2024-05-22 02:11:20 -0400

    commit

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const dataRoutes = require('./routes/dataRoutes');
+const index = require('./routes/index');
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', dataRoutes);
+app.use('', index);
 
 app.use((req, res, next) => {
     res.status(404).send("Sorry, can't find that!");
